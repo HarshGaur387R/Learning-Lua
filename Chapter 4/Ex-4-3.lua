4eb3611f7ab7e914dbdabd28ee585fda860f5545
Harsh Gaur <hgaur701@gmail.com>
2026-01-17 20:05:42 +0530

    chapter 4 Ex-4-3

--- Chapter 4/Ex-4-3.lua
+++ Chapter 4/Ex-4-3.lua
@@ -5,5 +5,22 @@ Exercise 4.3: Write a function to insert a string into a given position of anoth
 ]] --
 
 local function insert(str, n, sub)
+	local newString = '';
 
+	if (n == 1) then
+		newString = sub .. str;
+		return newString;
+	elseif (n == #str) then
+		newString = str .. sub;
+		return newString;
+	else
+		newString = str:sub(1, n - 1);
+		newString = newString .. sub .. str:sub(n, #str);
+		return newString;
+	end
 end
+
+
+print(insert("hello world", 7, "small "))  --> hello small world
+print(insert("hello world", 1, "start: ")) --> start: hello world
+print(insert("hello world", 11, "!"))      --> start: hello world
